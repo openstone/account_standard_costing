bde5d79c70826eed6c31d94210ceefef51a18f4e
openstone <oliver.yuan@gmail.com>
2013-02-01 17:19:55 +0800

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -24,21 +24,10 @@
     'author': 'Openstone, Oliver',
     'website': 'http://openerp.com - http://www.openstone.cn',
     'description': """
-This module supports the Anglo-Saxon accounting methodology by changing the accounting logic with stock transactions.
+标准成本法下，采购价格差异以及生产成本差异记账。
 =====================================================================================================================
 
-The difference between the Anglo-Saxon accounting countries and the Rhine 
-(or also called Continental accounting) countries is the moment of taking 
-the Cost of Goods Sold versus Cost of Sales. Anglo-Saxons accounting does 
-take the cost when sales invoice is created, Continental accounting will 
-take the cost at the moment the goods are shipped.
-
-This module will add this functionality by using a interim account, to 
-store the value of shipped goods and will contra book this interim 
-account when the invoice is created to transfer this amount to the 
-debtor or creditor account. Secondly, price differences between actual 
-purchase price and fixed product standard price are booked on a separate 
-account.""",
+略""",
     'images': ['images/account_standard_costing.jpeg'],
     'depends': ['product', 'purchase'],
     'category': 'Accounting & Finance',
